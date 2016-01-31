b6bb9a58e780f3aaa46cebf48d1387f793969cb3
Daniela Becerra <madajiji@gmail.com>
2016-01-31 00:56:53 -0600

    Change the list into  a dictionary, save email and name as key-value pairs

--- emailer.py
+++ emailer.py
@@ -4,17 +4,20 @@
 
 
 #create a new list for the emails
-emails = []
+emails = {}
 
 #open the file
 try:
     email_file = open('emails.txt', 'r')
     #insert the actual emails in our list without \n
     for line in email_file:
-            emails.append(line.strip())
+        (email, name) = line.split(',')
+        emails[email] = name.strip()
 except FileNotFoundError as err:
     print(err)
 
 
 
 print(emails)
+
+#min 10:55
