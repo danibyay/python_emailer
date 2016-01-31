84238fa805b3d66851b3b627fde339002ad18c50
Daniela Becerra <madajiji@gmail.com>
2016-01-31 01:40:28 -0600

    add requests module and get_weather_forecast function

--- emailer.py
+++ emailer.py
@@ -1,9 +1,10 @@
 #DANIELA BECERRA
 #This is an emailer app, to send emails to the spam van customers in
 #automated way
+import requests
 
 def get_emails():
-    #create a new list for the emails
+    #create a new dictionary for the emails
     emails = {}
 
     #open the file
@@ -19,7 +20,7 @@ def get_emails():
     return emails
 
 def get_schedule():
-    #open and read file
+    #open and read file for schedules
     try:
         schedule_file = open('schedule.txt', 'r')
         schedule = schedule_file.read()
@@ -28,10 +29,17 @@ def get_schedule():
 
     return schedule
 
+def get_weather_forecast():
+    url = 'http://api.openweathermap.org/data/2.5/weather?q=Guadalajara,mx&units=metric&appid=81f527ece7b1f5d6b47c23d1d6767c3d'
+    weather_request = requests.get(url)
+    weather_json = weather_request.json()
+    print(weather_json)
+
 def main():
     emails = get_emails()
     print(emails)
     schedule = get_schedule()
     print(schedule)
+    get_weather_forecast()
 
 main()
