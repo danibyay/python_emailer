d53c3220c9e81c0cb1ab15cfcc064f55839ab36c
Daniela Becerra <madajiji@gmail.com>
2016-01-31 02:07:30 -0600

    output formatted  weather forecast from api

--- emailer.py
+++ emailer.py
@@ -29,11 +29,17 @@ def get_schedule():
 
     return schedule
 
+#use the api of openweathermap to know the weather
 def get_weather_forecast():
     url = 'http://api.openweathermap.org/data/2.5/weather?q=Guadalajara,mx&units=metric&appid=81f527ece7b1f5d6b47c23d1d6767c3d'
-    weather_request = requests.get(url)
-    weather_json = weather_request.json()
-    print(weather_json)
+    weather_json = requests.get(url).json()
+    weather_description = weather_json['weather'][0]['description']
+    max_temp = weather_json['main']['temp_max']
+    min_temp = weather_json['main']['temp_min']
+    forecast = 'The Circus forecast for today is '
+    forecast += weather_description + ' with a high of '
+    forecast += str(max_temp) + ' and low of ' +  str(min_temp) + '.'
+    print(forecast)
 
 def main():
     emails = get_emails()
