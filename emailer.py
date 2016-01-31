2279ebe3bf3e883ea2af995beda8c1dd36969ea2
Daniela Becerra <madajiji@gmail.com>
2016-01-31 01:11:43 -0600

    make everything in functions, emails, schedule, and main()

--- emailer.py
+++ emailer.py
@@ -2,22 +2,36 @@
 #This is an emailer app, to send emails to the spam van customers in
 #automated way
 
+def get_emails():
+    #create a new list for the emails
+    emails = {}
 
-#create a new list for the emails
-emails = {}
+    #open the file
+    try:
+        email_file = open('emails.txt', 'r')
+        #insert the actual emails in our list without \n
+        for line in email_file:
+            (email, name) = line.split(',')
+            emails[email] = name.strip()
+    except FileNotFoundError as err:
+        print(err)
 
-#open the file
-try:
-    email_file = open('emails.txt', 'r')
-    #insert the actual emails in our list without \n
-    for line in email_file:
-        (email, name) = line.split(',')
-        emails[email] = name.strip()
-except FileNotFoundError as err:
-    print(err)
+    return emails
 
+def get_schedule():
+    #open and read file
+    try:
+        schedule_file = open('schedule.txt', 'r')
+        schedule = schedule_file.read()
+    except FileNotFoundError as err:
+        print(err)
 
+    return schedule
 
-print(emails)
+def main():
+    emails = get_emails()
+    print(emails)
+    schedule = get_schedule()
+    print(schedule)
 
-#min 10:55
+main()
